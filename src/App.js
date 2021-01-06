5c2674c1434d318465cedacac5db3e7f3de93716
nitinveeramalla <nitin_veera@yahoo.com>
2021-01-05 23:26:07 -0500

    Carousel Added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-
 //components
 import MyNavbar from "./components/my-navbar/my-navbar.component";
+import MyCarousel from './components/my-carousel/my-carousel.component';
+import TitleMessage from './components/title-message/title-message.component';
+
 const App = () => {
   return (
     <div>
         <MyNavbar />
-        My Portfolio
+        <MyCarousel />
+        <TitleMessage />
     </div>
   );
 }
