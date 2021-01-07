ba2e98ea5bfd92fec7b2892d44f9fa2ceae78e1f
nitinveeramalla <nitin_veera@yahoo.com>
2021-01-06 19:09:23 -0500

    all components added

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import Fade from 'react-reveal/Fade';
 import {Parallax} from 'react-parallax';
 import Container from 'react-bootstrap/Container';
 import Slide from "react-reveal/Slide";
+
 //components
 import MyNavbar from "./components/my-navbar/my-navbar.component";
 import MyCarousel from './components/my-carousel/my-carousel.component';
 import TitleMessage from './components/title-message/title-message.component';
 import About from './pages/about/about.component';
 import Skills from './pages/skills/skills.component'
+import TimeLine from "./components/project-timeline/project-timeline.component";
+import FooterPanel from "./components/footer/footer.component";
+import ContactForm from "./pages/contact-form/contact-form.component";
+import Particles from "react-particles-js";
+import { particlesOptions } from "./ParticleOptions";
 
 const App = () => {
   return (
     <div className="App" style={{ position: "relative" }}>
+        <MyNavbar />
         <MyCarousel />
         <TitleMessage />
-        <MyNavbar />
-        
+        {/*<Particles
+        className="particles particles-box"
+        params={particlesOptions}
+        />*/}
         {/* About section */}
         <div>
         <Parallax
@@ -35,12 +44,33 @@ const App = () => {
 
         {/* Skills Section */}
         
-        <Container className="container-box rounded">
+      <Container className="container-box rounded">
         <Slide bottom duration={500}>
           <hr />
           <Skills />
         </Slide>
       </Container>
+      
+      {/* Projects */}
+
+      <Container className="container-box rounded">
+        <Slide bottom duration={500}>
+          <hr />
+          <TimeLine />
+        </Slide>
+      </Container>
+
+      {/* Contact Form */}
+      
+      <Container className="container-box rounded">
+        <Fade duration={500}>
+          <hr />
+          <ContactForm />
+        </Fade>
+      </Container>
+        {/*Footer Panel */}
+      <hr />
+      <FooterPanel />
     </div>
   );
 }
--- src/components/my-carousel/my-carousel.component.jsx
+++ src/components/my-carousel/my-carousel.component.jsx
@@ -11,13 +11,13 @@ const MyCarousal = () => {
     <div id="home">
       <Carousel controls={false} indicators interval={2500} pause={false}>
         <Carousel.Item>
-          <img className="d-block w-100 custom-img" src={Slide2} alt="First slide" />
+          <img className="d-block w-100 custom-img" src={Slide1} alt="First slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100 custom-img" src={Slide2} alt="Third slide" />
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100 custom-img" src={Slide3} alt="Third slide" />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100 custom-img" src={Slide1} alt="Third slide" />
         </Carousel.Item>
       </Carousel>
       <ScrollDown />
